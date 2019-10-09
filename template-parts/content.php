a411b436afe5d9fa8335f8ab7f781fbac155fbb4
TakhribchiDev <takhribchidev@gmail.com>
2019-10-09 21:47:41 +0330

    Standard Post format template markup

---
+++ template-parts/content.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package sunsettheme
+ * -- Standard Post Format
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+
+		<?php the_title( '<h1 class="entry-title">', '</h1>' );?>
+
+		<div class="entry-meta">
+			<?php echo sunset_posted_meta(); ?>
+		</div>
+	</header>
+
+	<div class="entry-content">
+		<?php if ( has_post_thumbnail() ): ?>
+			<div class="standard-featured"><?php the_post_thumbnail(); ?></div>
+		<?php endif; ?>
+
+        <div class="entry-excerpt">
+            <?php the_excerpt(); ?>
+        </div><!-- .entry-excerpt -->
+
+        <div class="button-container">
+            <a href="<?php the_permalink(); ?>" class="btn btn-default btn-secondary"><?php _e( 'Read More' ); ?></a>
+        </div><!-- .button-container-->
+	</div><!-- .entry-content -->
+
+    <div class="entry-footer">
+        <?php echo sunset_posted_footer(); ?>
+    </div><!-- .entry-footer -->
+
+</article>
