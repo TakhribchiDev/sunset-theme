8797c585939268ee4b644f9737d940008dd1f8d3
TakhribchiDev <takhribchidev@gmail.com>
2019-10-09 21:41:45 +0330

    Add functions to print post Date, Categories, Tags and Comments count

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -34,3 +34,46 @@ function sunset_register_nav_menu() {
 add_action( 'after_setup_theme', 'sunset_register_nav_menu' );
 
 add_theme_support( 'post-thumbnails' );
+
+/**
+ * @package sunsettheme
+ * =========================
+ *    BLOG LOOP CUSTOM FUNCTIONS
+ * =========================
+ */
+
+function sunset_posted_meta() {
+	$posted_on = human_time_diff( get_the_time( 'U' ), current_time( 'timestamp' ) );
+
+	$categories = get_the_category();
+	$separator = ', ';
+	$output = '';
+	$i = 1;
+
+	if ( !empty($categories) ):
+		foreach ( $categories as $category ):
+			if ( $i > 1 ): $output .= $separator; endif;
+			$output .= '<a href="' . esc_url( get_category_link( $category->term_id  ) ) . '"  alt="' . esc_attr( 'View all posts in %s', $category->name ) . '">' . esc_html( $category->name ) . '</a>';
+			$i++;
+		endforeach;
+	endif;
+
+	return '<span class="posted-on">Posted <a href="' . esc_url( get_permalink() ) . '">' . $posted_on . '</a> ago</span> / <span class="posted-in">' . $output . '</span>';
+}
+
+function sunset_posted_footer() {
+	$comments_num = get_comments_number();
+	if ( comments_open() ) {
+		if ( $comments_num == 0 ) {
+			$comments = __( 'No Comments' );
+		} elseif ( $comments_num > 1 ) {
+			$comments = __( $comments_num . ' Comments' );
+		} else {
+			$comments = __( '1 Comment' );
+		}
+		$comments = '<a href="' . get_comments_link() . '">' . $comments . '<span class="sunset-icon sunset-comment"></span></a>';
+	} else {
+		$comments = __( 'Comments are closed' );
+	}
+	return '<div class="post-footer-container"><div class="row"><div class="col-xs-12 col-sm-6">' . get_the_tag_list( '<div class="tags-list"><span class="sunset-icon sunset-tag"></span>', ' ', '</div>' ) . '</div><div class="col-xs-12 col-sm-6">'. $comments .'</div></div></div>';
+}
